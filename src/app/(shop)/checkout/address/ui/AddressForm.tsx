597988fb3070f434efa688c0639639326c17a385
Daryhen-dev <daryhen.work@gmail.com>
2025-12-05 16:57:11 -0500

    order

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -60,8 +60,8 @@ export const AddressForm = ({countries, userStoredAddress = {}}:Props) => {
   
 
   const onSubmit = async (data: FormInputs) => {
-    setAddress(data)
     const {rememberAddress, ...restAddress} = data;
+    setAddress(restAddress)
 
     if (data.rememberAddress) {
       await setUserAddress(restAddress, session?.user?.id || '' );
